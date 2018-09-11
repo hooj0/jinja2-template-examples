c67716fe4e5f002d2b7f05fecf231c734369cc1a
hoojo <hoojo@qq.com>
2018-09-11 10:53:43 +0800

    :sparkles: :bento: :recycle:  Changed comment

--- 06_filter_set_ns/filter.py
+++ 06_filter_set_ns/filter.py
@@ -5,18 +5,38 @@
 # @github:   https://github.com/hooj0
 # @create date: 2018-09-10
 # @copyright by hoojo @2018
-# @changelog
+# @changelog jinja2 template filter execution inner func
 
 
 # ===============================================================================
-# 标题：
+# 标题：template filter execution inner func
 # ===============================================================================
-# 使用：
+# 使用：使用 filter 关键字，处理标签块内部中的数据
 # -------------------------------------------------------------------------------
-# 描述：
+# 描述：filter 标签块允许处理内部的字符串，进行内部函数的处理
+# -------------------------------------------------------------------------------
+# 内置过滤器列表：http://jinja.pocoo.org/docs/2.10/templates/#list-of-builtin-filters
 # -------------------------------------------------------------------------------
 
 
 # -------------------------------------------------------------------------------
-#
+# filter 标签块的字符串转换演示
 # -------------------------------------------------------------------------------
+from jinja2 import Environment, select_autoescape, FileSystemLoader
+
+# 从指定位置加载模板的环境
+loader = FileSystemLoader('.')
+# 更多配置参考：http://jinja.pocoo.org/docs/2.10/api/#high-level-api
+env = Environment(loader=loader,
+                  autoescape=select_autoescape(['html', 'xml']),
+                  line_statement_prefix="#",
+                  line_comment_prefix="##",
+                  trim_blocks=True, keep_trailing_newline=True, lstrip_blocks=True)
+
+# 获取模板
+template = env.get_template('filter-template.html')
+
+# 渲染模板
+result = template.render()
+
+print(result)
--- 06_filter_set_ns/set.py
+++ 06_filter_set_ns/set.py
@@ -5,18 +5,37 @@
 # @github:   https://github.com/hooj0
 # @create date: 2018-09-11
 # @copyright by hoojo @2018
-# @changelog
+# @changelog inner scope variable assignments
 
 
 # ===============================================================================
-# 标题：
+# 标题：inner scope variable assignments
 # ===============================================================================
-# 使用：
+# 使用： {% set key, value = {'key': 2334} %}
 # -------------------------------------------------------------------------------
-# 描述：
+# 描述： 可以在部分流程语句或模板中定义局部的变量进行引用使用
 # -------------------------------------------------------------------------------
 
 
 # -------------------------------------------------------------------------------
-#
+# 声明简单的变量进行使用
 # -------------------------------------------------------------------------------
+from jinja2 import Environment, select_autoescape, FileSystemLoader
+
+# 从指定位置加载模板的环境
+loader = FileSystemLoader('.')
+# 更多配置参考：http://jinja.pocoo.org/docs/2.10/api/#high-level-api
+env = Environment(loader=loader,
+                  autoescape=select_autoescape(['html', 'xml']),
+                  line_statement_prefix="#",
+                  line_comment_prefix="##",
+                  trim_blocks=True, keep_trailing_newline=True, lstrip_blocks=True)
+
+
+# 获取模板
+template = env.get_template('set-template.html')
+
+# 渲染模板
+result = template.render()
+
+print(result)
