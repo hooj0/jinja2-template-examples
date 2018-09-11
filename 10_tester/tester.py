61b2eb6a2e8b526129c5d92188e2c13c21a3ac03
hoojo <hoojo@qq.com>
2018-09-11 19:15:21 +0800

    :sparkles: :bento:  Added tester py

--- 10_tester/tester.py
+++ 10_tester/tester.py
@@ -9,14 +9,39 @@
 
 
 # ===============================================================================
-# 标题：
+# 标题：tests if process statement
 # ===============================================================================
-# 使用：
+# 使用：{% if var is defined %}
 # -------------------------------------------------------------------------------
-# 描述：
+# 描述：测试过滤器用于在 if 流程语句中作为判断的条件依据
+# http://jinja.pocoo.org/docs/2.10/templates/#list-of-builtin-tests
 # -------------------------------------------------------------------------------
 
 
 # -------------------------------------------------------------------------------
-#
+# 测试过滤器
 # -------------------------------------------------------------------------------
+from jinja2 import Environment, select_autoescape, FileSystemLoader
+
+# 从指定位置加载模板的环境
+loader = FileSystemLoader('.')
+# 更多配置参考：http://jinja.pocoo.org/docs/2.10/api/#high-level-api
+env = Environment(loader=loader,
+                  autoescape=select_autoescape(['html', 'xml']),
+                  line_statement_prefix="#",
+                  line_comment_prefix="##",
+                  trim_blocks=True, keep_trailing_newline=True, lstrip_blocks=True)
+
+# 获取模板
+template = env.get_template('tester-template.html')
+
+
+class User():
+    name = "hoojo"
+    age = 23
+    __flag = 0
+
+# 渲染模板
+result = template.render(user = User(), users = [ User(), User() ])
+
+print(result)
