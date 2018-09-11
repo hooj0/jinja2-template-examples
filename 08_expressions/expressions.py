a29ac4e1da7b53e80cc77002cba5dd7547c5a921
hoojo <hoojo@qq.com>
2018-09-11 16:09:28 +0800

    :sparkles: :bento: :recycle:  Changed expression-template html

--- 08_expressions/expressions.py
+++ 08_expressions/expressions.py
@@ -5,18 +5,38 @@
 # @github:   https://github.com/hooj0
 # @create date: 2018-09-11
 # @copyright by hoojo @2018
-# @changelog
+# @changelog jinja allows basic expressions everywhere
 
 
 # ===============================================================================
-# 标题：
+# 标题：template expression
 # ===============================================================================
 # 使用：
 # -------------------------------------------------------------------------------
-# 描述：
+# 描述：表达式
 # -------------------------------------------------------------------------------
 
 
 # -------------------------------------------------------------------------------
 #
 # -------------------------------------------------------------------------------
+from jinja2 import Environment, select_autoescape, FileSystemLoader
+
+# 从指定位置加载模板的环境
+loader = FileSystemLoader('.')
+# 更多配置参考：http://jinja.pocoo.org/docs/2.10/api/#high-level-api
+env = Environment(loader=loader,
+                  autoescape=select_autoescape(['html', 'xml']),
+                  line_statement_prefix="#",
+                  line_comment_prefix="##",
+                  trim_blocks=True, keep_trailing_newline=True, lstrip_blocks=True)
+
+
+# 获取模板
+template = env.get_template('expression-template.html')
+
+# 渲染模板
+# 默认情况下，包含的模板可以访问活动上下文的变量
+result = template.render(var = "context data", boxes = ["foo", "bar"])
+
+print(result)
